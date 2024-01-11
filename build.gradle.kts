8cf3ef918662be0a03340dfc7f97262125df5f8d
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2024-01-11 19:29:37 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+}
