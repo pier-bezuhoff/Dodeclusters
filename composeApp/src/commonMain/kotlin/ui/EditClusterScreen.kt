f2d21817ac414f6dc4fcf6fffdad4927b6e14599
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2024-01-20 05:28:49 +0300

    proper create>things dropdown menu (but idt i need it)

--- composeApp/src/commonMain/kotlin/ui/EditClusterScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/EditClusterScreen.kt
@@ -1,13 +1,18 @@
 package ui
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.BottomAppBar
@@ -40,6 +45,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.drawBehind
@@ -53,6 +60,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupPositionProvider
@@ -109,7 +117,9 @@ fun EditClusterTopBar(
 }
 
 enum class CreateAction {
-    CIRCLE_BY_CENTER_AND_RADIUS, CIRCLE_BY_3_POINTS, LINE_BY_2_POINTS
+    CIRCLE_BY_CENTER_AND_RADIUS,
+//    CIRCLE_BY_3_POINTS,
+    LINE_BY_2_POINTS
 }
 
 data class CreateActionParams(
@@ -118,34 +128,28 @@ data class CreateActionParams(
     val callback: () -> Unit,
 )
 
-@OptIn(ExperimentalResourceApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun EditClusterBottomBar(
     viewModel: EditClusterViewModel,
 ) {
+    // create x = a new mode
     var createMenuIsExpanded by remember { mutableStateOf(false) }
     var createAction by remember { mutableStateOf(CreateAction.CIRCLE_BY_CENTER_AND_RADIUS) }
     val createActionParams = remember { mapOf(
-        CreateAction.CIRCLE_BY_CENTER_AND_RADIUS to CreateActionParams(Icons.Default.AddCircle, "C<-c&R aksjdf;lakjsdfk;lajslk;fjsad") {},
-        CreateAction.CIRCLE_BY_3_POINTS to CreateActionParams(Icons.Default.AccountCircle, "C<-3P") {},
-        CreateAction.LINE_BY_2_POINTS to CreateActionParams(Icons.Default.ArrowForward, "L<-2P") {},
+        CreateAction.CIRCLE_BY_CENTER_AND_RADIUS to CreateActionParams(Icons.Default.AddCircle, "Circle by center and radius") { println("c1"); viewModel.newCircle() },
+//        CreateAction.CIRCLE_BY_3_POINTS to CreateActionParams(Icons.Default.AccountCircle, "Circle by 3 points") { println("c2") },
+        CreateAction.LINE_BY_2_POINTS to CreateActionParams(Icons.Default.ArrowForward, "Line by 2 points") { println("l1") },
     ) }
     BottomAppBar {
-//        IconButton(onClick = viewModel::newCircle) {
-//            Icon(Icons.Default.AddCircle, contentDescription = "new circle")
-//        }
-
-        Box() {
-            Row(Modifier) {
-                IconButton(
-                    onClick = {
-                          createMenuIsExpanded = true
+        Box {
+            IconButton(
+                onClick = {
 //                        createActionParams[createAction]!!.callback
-                    },
-                ) {
-                    Icon(createActionParams[createAction]!!.icon, contentDescription = createActionParams[createAction]!!.contentDescription)
+                    createMenuIsExpanded = true
                 }
-                ExposedDropdownMenuDefaults.TrailingIcon(createMenuIsExpanded)
+            ) {
+                Icon(createActionParams[createAction]!!.icon, contentDescription = createActionParams[createAction]!!.contentDescription)
             }
             DropdownMenu(
                 expanded = createMenuIsExpanded,
@@ -161,56 +165,16 @@ fun EditClusterBottomBar(
                             createMenuIsExpanded = false
                             callback()
                         },
-                        Modifier.wrapContentWidth(),
+//                        Modifier.wrapContentWidth(),
                     ) {
-                        Row(Modifier.wrapContentWidth()) {
+                        Row() {
                             Icon(icon, contentDescription = contentDescription)
-                            Text(contentDescription, overflow = TextOverflow.Visible, softWrap = false, maxLines = 1)
+                            Text(contentDescription, Modifier.padding(4.dp), overflow = TextOverflow.Visible, softWrap = false, maxLines = 1)
                         }
                     }
                 }
             }
         }
-
-//        ExposedDropdownMenuBox(
-//            expanded = createMenuIsExpanded,
-//            onExpandedChange = {
-//                createMenuIsExpanded = !createMenuIsExpanded
-//            }
-//        ) {
-//            Row() {
-//                IconButton(
-//                    onClick = createActionParams[createAction]!!.callback,
-//                ) {
-//                    Icon(createActionParams[createAction]!!.icon, contentDescription = createActionParams[createAction]!!.contentDescription)
-//                }
-//                ExposedDropdownMenuDefaults.TrailingIcon(createMenuIsExpanded)
-//            }
-//            ExposedDropdownMenu(
-//                expanded = createMenuIsExpanded,
-//                onDismissRequest = {
-//                    createMenuIsExpanded = false
-//                },
-//                Modifier.exposedDropdownSize(false)
-//            ) {
-//                CreateAction.entries.forEach { action ->
-//                    val (icon, contentDescription, callback) = createActionParams[action]!!
-//                    DropdownMenuItem(
-//                        onClick = {
-//                            createAction = action
-//                            createMenuIsExpanded = false
-//                            callback()
-//                        },
-//                        Modifier.wrapContentWidth(),
-//                    ) {
-//                        Row(Modifier.wrapContentWidth()) {
-//                            Icon(icon, contentDescription = contentDescription)
-//                            Text(contentDescription, overflow = TextOverflow.Visible, softWrap = false, maxLines = 1)
-//                        }
-//                    }
-//                }
-//            }
-//        }
 
         IconButton(onClick = viewModel::copyCircles) {
             Icon(painterResource("icons/copy.xml"), contentDescription = "copy circle(s)")
