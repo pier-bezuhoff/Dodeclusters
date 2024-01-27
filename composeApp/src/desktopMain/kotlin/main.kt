5aed2b8705316a397b3716dbc28b39de64edfa73
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2024-01-27 09:31:08 +0300

    added desktop icons (not very good, test more)

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,5 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
@@ -7,10 +8,12 @@ import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     val windowState = rememberWindowState(placement = WindowPlacement.Maximized)
+    val icon = painterResource("icon-128.png") // for w/e reason looks awful
     Window(
         state = windowState,
         onCloseRequest = ::exitApplication,
-        title = "Dodeclusters"
+        title = "Dodeclusters",
+        icon = icon,
     ) {
         App()
     }
