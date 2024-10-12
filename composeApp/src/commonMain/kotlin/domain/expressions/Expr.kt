6e41063b7e83cd60cccb3f3466e177a9e4bf4e2f
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2024-10-12 08:59:00 +0300

    fixed serialization error on mobile

--- composeApp/src/commonMain/kotlin/domain/expressions/Expr.kt
+++ composeApp/src/commonMain/kotlin/domain/expressions/Expr.kt
@@ -19,59 +19,71 @@ sealed class Expr(
 ) {
     @Serializable
     sealed class OneToOne(
+        @SerialName("params")
         override val parameters: Parameters,
         override val args: List<Indexed>,
     ) : Expr(parameters, args)
     @Serializable
     sealed class OneToMany(
+        @SerialName("params")
         override val parameters: Parameters,
         override val args: List<Indexed>,
     ) : Expr(parameters, args)
 
     // NOTE: proper handling of dependent carrier requires computation of inverse function for any expr
     //  p' = f(Δ(f⁻¹(p)), where point p on dependent carrier f(<free>) moves to p' when <free> is affected by Δ
+    @Serializable
     data class Incidence(
         override val parameters: IncidenceParameters,
         val carrier: Indexed.Circle,
     ) : OneToOne(parameters, listOf(carrier))
+    @Serializable
     data class CircleByCenterAndRadius(
         val center: Indexed.Point,
         val radiusPoint: Indexed.Point
     ) : OneToOne(Parameters.None, listOf(center, radiusPoint))
+    @Serializable
     data class CircleBy3Points( // order-less
         val point1: Indexed,
         val point2: Indexed,
         val point3: Indexed,
     ) : OneToOne(Parameters.None, listOf(point1, point2, point3))
+    @Serializable
     data class CircleByPencilAndPoint(
         val circle1: Indexed,
         val circle2: Indexed,
         val point: Indexed,
     ) : OneToOne(Parameters.None, listOf(circle1, circle2, point))
+    @Serializable
     data class LineBy2Points( // order-less
         val point1: Indexed,
         val point2: Indexed,
     ) : OneToOne(Parameters.None, listOf(point1, point2))
+    @Serializable
     data class CircleInversion(
         val target: Indexed,
         val engine: Indexed.Circle,
     ) : OneToOne(Parameters.None, listOf(target, engine))
 
+    @Serializable
     data class Intersection( // order-less
         val circle1: Indexed.Circle,
         val circle2: Indexed.Circle,
     ) : OneToMany(Parameters.None, listOf(circle1, circle2))
     // TODO: point-point line interpolation
+    @Serializable
     data class CircleInterpolation(
         override val parameters: InterpolationParameters,
         val startCircle: Indexed.Circle,
         val endCircle: Indexed.Circle,
     ) : OneToMany(parameters, listOf(startCircle, endCircle))
+    @Serializable
     data class CircleExtrapolation(
         override val parameters: ExtrapolationParameters,
         val startCircle: Indexed.Circle,
         val endCircle: Indexed.Circle,
     ) : OneToMany(parameters, listOf(startCircle, endCircle))
+    @Serializable
     data class LoxodromicMotion(
         override val parameters: LoxodromicMotionParameters,
         val divergencePoint: Indexed.Point,
@@ -155,6 +167,7 @@ sealed class Expr(
     }
 }
 
+@Serializable
 data class IncidenceParameters(
     val order: Double
 ) : Parameters
